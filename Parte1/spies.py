ab53681d7cb3b5c66f32f873a567a2ec106c5249
maxionbit <shalnark@localhost.localdomain>
2018-05-12 14:21:04 -0300

    Parte 1, BFS

--- Parte1/spies.py
+++ Parte1/spies.py
@@ -4,41 +4,89 @@ import sys
 import argparse
 
 class Node:
-    def __init__(self, x, y):
-        self._x = x
-        self._y = y
+	def __init__(self, x, y):
+		self._x = x
+		self._y = y
 
-    def get_x(self):
+	def get_x(self):
 		return self._x
 
-    def get_y(self):
+	def get_y(self):
 		return self._y
+
+	def __str__(self):
+		return "x: " + str(self._x) + ", y: " + str(self._y)
+
+	def __hash__(self):
+		return hash(self._x)+hash(self._y)
+
+	def __eq__(self, other):
+		return (
+			self.__class__ == other.__class__ and
+			self._x == other._x and
+			self._y == other._y
+		)
+
+	def __ne__(self, other):
+		return (
+			self.__class__ != other.__class__ or
+			self._x != other._x or
+			self._y != other._y
+		)
 
 #	def get_distance(self, node):
 #		"""Distancia euclidea"""
 #		return 0
 
 #Grafo del mapa
-map_graph = {}
-discovered_nodes = {} #set de nodos ya visitados
-layers = [] #las capas del arbol BFS
-bfs_tree = {} #el arbol BFS
+map_graph = {} #sera un diccionario: clave= nodo, valor= conjunto de nodos adyacentes
 
-def BFS(start_node, end_node):
+#O(V + E)
+def BFS(map_graph, start_node, end_node):
+	bfs_tree = {} #el arbol BFS
+	discovered_nodes = set() #set de nodos ya visitados
+	layers = [set()] #lista de capas
+	layers[0]= {start_node} #capa inicial
 	layer_index = 0
-	layers[0].add({start_node}) #capa inicial
+	new_nodes_discovered = True
+#	print "BFS: map graph => " 
+#	print map_graph
+	#print "BFS: layers => "
+	#print layers
 
-	#build the next layer
-	for node_i in layers[layer_index]:
-		next_level_nodes = map_graph[node_i] #set of adjacent nodes
-		for adj_node in next_level_nodes:
-			if adj_node not in discovered_nodes:
-				print str(adj_node.get_x())+', '+str(adj_node.get_y())
-				layers[layer_index+1].add(adj_node)
-				discovered_nodes.add(adj_node)
-		layer_index += 1
+	print start_node
+	print end_node
 
-	#return distance
+	while new_nodes_discovered:
+		new_nodes_discovered = False
+		for current_layer_node_i in layers[layer_index]: #se recorre la capa actual
+			next_level_nodes_set = map_graph[current_layer_node_i] #set de nodos adyacentes
+			print "BFS: next level nodes set => "
+			print next_level_nodes_set
+
+			layers.append(set())
+			for adj_node in next_level_nodes_set: #se recorren todos los adyacentes
+				if adj_node not in discovered_nodes:
+					new_nodes_discovered = True
+					print "BFS: Se visita un nuevo nodo: " + str(adj_node.get_x())+', '+str(adj_node.get_y())
+					layers[layer_index+1].add(adj_node) #se va armando la siguiente capa
+
+					#print "BFS: layers => "
+					#print layers
+
+					discovered_nodes.add(adj_node)
+					if current_layer_node_i not in bfs_tree:
+						bfs_tree.update({current_layer_node_i:{adj_node}}) #se va armando el arbol BFS
+					else:
+						bfs_tree[current_layer_node_i].add(adj_node) #se va armando el arbol BFS
+					if adj_node == end_node:
+						return layer_index + 1
+			layer_index += 1
+
+	return -1
+
+def load_map(map_graph):
+	
 
 def main():
 	print "TP2 - Parte 1: Spy vs Spy"
@@ -52,34 +100,36 @@ def main():
 	parser.add_argument("airport_y", help="y coord for airport", type=int)
 
 	args = parser.parse_args()
-	#print str(args.spy1_x) + " " + str(args.spy1_y)
 	
-	#leer grafo de mapa.coords y cargarlo en memoria
-
+	#TODO leer grafo de mapa.coords y cargarlo en memoria
+	#load_map(map_graph)
 	node_i = Node(1,1) #spy 1
 	node_j = Node(2,2)
 	node_k = Node(3,3)
 	node_l = Node(4,4) #airport
 	node_m = Node(2,1) #spy 2
 	node_n = Node(10,29)
-	map_graph = {node_i:{node_j, node_l},
-				 node_j:{node_k},
-				 node_k:{node_l},
-				 node_m:{node_n},
-				 node_n:{node_l}}
-	
-	#spy1_edges_count = BFS(spy1, airport)
-	#spy2_edges_count = BFS(spy2, airport)
-#	spy1_edges_count = BFS(node_i, node_l)
-#	spy2_edges_count = BFS(node_m, node_l)
-	"""
-	if spy1_edges_count < sp2_edges_count:
+	map_graph = {node_i:{node_j, node_m},
+				 node_j:{node_k, node_i},
+				 node_k:{node_j, node_l},
+				 node_m:{node_i, node_n},
+				 node_n:{node_l, node_m}}
+
+	#spy1_edges_count = BFS(graph, spy1, airport)
+	spy1_edges_count = BFS(map_graph, node_i, node_l)
+	print "spy1_edges_count = "+ str(spy1_edges_count)
+
+	#spy2_edges_count = BFS(graph, spy2, airport)
+	spy2_edges_count = BFS(map_graph, node_m, node_l)
+	print "spy2_edges_count = "+ str(spy2_edges_count)
+
+	if spy1_edges_count < spy2_edges_count:
 		print "Spy1 wins!"
 	else:
-		if spy1_edges_count > sp2_edges_count:
+		if spy1_edges_count > spy2_edges_count:
 			print "Spy2 wins!"
 		else:	
 			print "The spies tie!"
-	"""
+	
 if __name__ == "__main__":
 	main()
