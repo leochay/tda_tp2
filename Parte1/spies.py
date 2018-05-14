e14dced626d0434aa3762f38d421d37e1c11bb37
maxiongithub <shalnark@localhost.localdomain>
2018-05-13 22:05:12 -0300

    Versión final con Dijkstra

--- Parte1/spies.py
+++ Parte1/spies.py
@@ -2,6 +2,8 @@
 
 import sys
 import argparse
+import math
+import heapq
 
 class Node:
 	def __init__(self, x, y):
@@ -49,22 +51,23 @@ def BFS(map_graph, start_node, end_node):
 	layer_index = 0
 	new_nodes_discovered = True
 
-	print start_node
-	print end_node
+	print "******* Se ejecuta BFS ***********"
+	print "Nodo inicial: " + str(start_node)
+	print "Nodo final: " + str(end_node)
 
 	while new_nodes_discovered:
 		new_nodes_discovered = False
-		print "Start layer " + str(layer_index)
+		#print "Start layer " + str(layer_index)
 		for current_layer_node_i in layers[layer_index]: #se recorre la capa actual
 			next_level_nodes_set = map_graph[current_layer_node_i] #set de nodos adyacentes
-			print "BFS: next level nodes set => "
-			print next_level_nodes_set
+			#print "BFS: next level nodes set => "
+			#print next_level_nodes_set
 
 			layers.append(set())
 			for adj_node in next_level_nodes_set: #se recorren todos los adyacentes
 				if adj_node not in discovered_nodes:
 					new_nodes_discovered = True
-					print "BFS: Se visita un nuevo nodo: " + str(adj_node.get_x())+', '+str(adj_node.get_y())
+					#print "BFS: Se visita un nuevo nodo: " + str(adj_node.get_x())+', '+str(adj_node.get_y())
 					layers[layer_index+1].add(adj_node) #se va armando la siguiente capa
 					discovered_nodes.add(adj_node)
 					if current_layer_node_i not in bfs_tree:
@@ -76,6 +79,34 @@ def BFS(map_graph, start_node, end_node):
 		layer_index += 1
 
 	return -1
+
+def dijkstra(map_graph, start_node, end_node):
+	nodes_distances = {} #diccionario de distancias
+	nodes_distances[start_node] = 0
+
+	print "******* Se ejecuta Dijkstra ***********"
+	print "Nodo inicial: " + str(start_node)
+	print "Nodo final: " + str(end_node)
+
+	priority_queue = []
+	heapq.heappush(priority_queue, (0, start_node))
+
+	while priority_queue:
+		node_tuple_i = heapq.heappop(priority_queue)
+		if end_node == node_tuple_i[1]:
+			#print "BFS: se visito el nodo final!"
+			return node_tuple_i[0]
+		#print "Nodo actual ----> " + str( node_tuple_i[1])
+		next_level_nodes_set = map_graph[node_tuple_i[1]]
+		for adj_node in next_level_nodes_set: #se recorren los adyacentes
+			#Se actualiza la distancia al nodo adyacente de ser necesario
+			new_distance = nodes_distances[node_tuple_i[1]] + adj_node.get_distance(node_tuple_i[1])
+			if (adj_node not in nodes_distances) or (new_distance < nodes_distances[adj_node]):
+				#Se encontro una distancia menor
+				nodes_distances[adj_node] = new_distance
+				heapq.heappush(priority_queue, (new_distance, adj_node))
+				
+	return 0
 
 def load_map_from_file(map_graph):
 	with open('mapa.coords') as fp:
@@ -94,28 +125,17 @@ def load_map_from_file(map_graph):
 			else:
 				map_graph[new_node_2].add(new_node_1)
 
-	'''
-	print "Map graph caso de prueba:"
+def check_algorithm(value):
+	valid_algorithms = ['B', 'D']
+	if value not in valid_algorithms:
+		raise argparse.ArgumentTypeError("%s no es un algoritmo valido. Usar B o D. Ver -h" % value)
+	return value
 
-	node_i = Node(1,1) #spy 1
-	node_j = Node(2,2)
-	node_k = Node(3,3)
-	node_l = Node(4,4) #airport
-	node_m = Node(2,1) #spy 2
-	node_n = Node(10,29)
-
-	map_graph = {node_i:{node_j, node_m},
-				 node_j:{node_k, node_i},
-				 node_k:{node_j, node_l},
-				 node_m:{node_i, node_n},
-				 node_n:{node_l, node_m}}
-	print map_graph
-	'''
-	
 def main():
 	print "TP2 - Parte 1: Spy vs Spy"
 	
 	parser = argparse.ArgumentParser()
+	parser.add_argument("algorithm", help="algorithm to use: B for BFS, D for Dijkstra", type=check_algorithm)
 	parser.add_argument("spy1_x", help="x coord for spy 1", type=int)
 	parser.add_argument("spy1_y", help="y coord for spy 1", type=int)
 	parser.add_argument("spy2_x", help="x coord for spy 2", type=int)
@@ -130,21 +150,23 @@ def main():
 	airport = Node(int(args.airport_x), int(args.airport_y))
 	load_map_from_file(map_graph)
 
-	spy1_edges_count = BFS(map_graph, spy1, airport)
-	#spy1_edges_count = BFS(map_graph, node_i, node_l)
-	print "spy1_edges_count = "+ str(spy1_edges_count)
+	if args.algorithm == 'B':
+		spy1_edges_count = BFS(map_graph, spy1, airport)	
+		spy2_edges_count = BFS(map_graph, spy2, airport)	
+	else:
+		spy1_edges_count = dijkstra(map_graph, spy1, airport)
+		spy2_edges_count = dijkstra(map_graph, spy2, airport)
 
-	spy2_edges_count = BFS(map_graph, spy2, airport)
-	#spy2_edges_count = BFS(map_graph, node_m, node_l)
-	print "spy2_edges_count = "+ str(spy2_edges_count)
+	print "Distancia del esp"+u'\u00ed'+"a 1 al aeropuerto = "+ str(spy1_edges_count)
+	print "Distancia del esp"+u'\u00ed'+"a 2 al aeropuerto = "+ str(spy2_edges_count)
 
 	if spy1_edges_count < spy2_edges_count:
-		print "Spy1 wins!"
+		print "El esp"+u'\u00ed'+"a 1 llegar"+u'\u00e1'+ " antes al aeropuerto."
 	else:
 		if spy1_edges_count > spy2_edges_count:
-			print "Spy2 wins!"
+			print "El esp"+u'\u00ed'+"a 2 llegar"+u'\u00e1'+ " antes al aeropuerto."
 		else:	
-			print "The spies tie!"
+			print "Los 2 esp"+u'\u00ed'+"as llegar"+u'\u00e1'+"n al mismo tiempo."
 
 if __name__ == "__main__":
 	main()
